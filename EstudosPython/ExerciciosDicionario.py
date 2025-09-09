d77230b3bd5adebcb09526d69fcdcc3f6c1045e1
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2025-09-09 17:09:56 -0300

    Add files via upload

---
+++ EstudosPython/ExerciciosDicionario.py
@@ -0,0 +1,41 @@
+"""#Exercício 1
+jogador = {"nome": "João", "pontuação": "9", "nivel": "12"}
+
+print(jogador)
+print(f"Pontuação: {jogador["pontuação"]}\n")
+
+#Exercício 2
+filme = {"titulo": "Carros 2", "diretor": "John Lasseter", "ano_lancamento": "2011", "genero": "Família/Comédia"}
+
+print(f"Filme: {filme["titulo"]}\nDiretor: {filme["diretor"]}\n")
+
+#Exercício 3
+funcionario = {"id": "95423", "nome": "João", "cargo": "Atendente", "salario": "R$2.000"}
+
+print(f"Nome: {funcionario["nome"]}\nCargo: {funcionario["cargo"]}\n")
+
+#Exercício 4
+animal_estimacao = {"nome": "Amante", "especie": "Cavalo", "idade": "6 anos", "cor_pelo": "Marrom"}
+
+print(f"Espécie: {animal_estimacao['especie']}\nIdade: {animal_estimacao['idade']}\n")
+
+#Exercício 5
+receita = {"nome_prato": "Carne de porco", "tempo_preparo": "6 minutos"}
+
+print(f"Nome do prato: {receita['nome_prato']}\nTempo de preparo: {receita['tempo_preparo']}")
+
+#Exercício 6
+jogador = {"nome": "Heroi", "pontuação": 1000, "nivel": 1}
+
+jogador["pontuação"] = 1250
+jogador["nivel"] = 2
+
+print(f"{jogador}\n")
+
+#Exercício 7
+livro = {"titulo": "Kimetsu no yaiba", "autor": "Koyoharu Gotouge", "estoque": 5}
+
+livro["estoque"] += 10
+livro["preco"] = "R$89,90"
+
+print(livro)"""
